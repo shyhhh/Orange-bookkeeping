faa32b4f619d27e9ae0a26074646035372dd1862
shyhhh <hhhtheshy@gmail.com>
2023-02-09 14:04:03 +0800

    添加 build base，添加自动部署到 GitHub 的脚本

---
+++ bin/deploy_to_github.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+rm -rf dist
+npm run build
+cd dist
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:shyhhh/Orange-bookkeeping.git main:preview-pages
+cd -
