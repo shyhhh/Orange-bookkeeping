e2f84fffe351bbfb62e2f4dfe4699b5eb8030f7a
shyhhh <hhhtheshy@gmail.com>
2023-02-15 11:05:38 +0800

    配置 unocss

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
+import 'virtual:uno.css'
+import './routes/global.scss'
 
 const div = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(div)
