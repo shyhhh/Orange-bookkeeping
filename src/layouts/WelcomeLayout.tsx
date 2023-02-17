e58c386be27dcf79e60234173c7aab57deb3ff8b
shyhhh <hhhtheshy@gmail.com>
2023-02-17 17:15:50 +0800

    用户手势可以进入下一页

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,8 +1,9 @@
 import { animated, useTransition } from '@react-spring/web'
-import type { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
 import { useRef, useState } from 'react'
-import { Link, useLocation, useOutlet } from 'react-router-dom'
+import { Link, useLocation, useNavigate, useOutlet } from 'react-router-dom'
 import logo from '../assets/images/logo.svg'
+import { useSwipe } from '../hooks/useSwipe'
 const linkMap: Record<string, string> = {
   '/welcome/1': '/welcome/2',
   '/welcome/2': '/welcome/3',
@@ -11,11 +12,12 @@ const linkMap: Record<string, string> = {
 }
 
 export const WelcomeLayout: React.FC = () => {
+  const animating = useRef(false)
   const map = useRef<Record<string, ReactNode>>({})
   const location = useLocation()
   const outlet = useOutlet()
   map.current[location.pathname] = outlet
-  const [extraStyle, setExtraStyle] = useState < { position: 'relative' | 'absolute' } >({ position: 'relative' })
+  const [extraStyle, setExtraStyle] = useState<{ position: 'relative' | 'absolute' }>({ position: 'relative' })
   const transitions = useTransition(location.pathname, {
     from: {
       transform: location.pathname === '/welcome/1'
@@ -29,21 +31,32 @@ export const WelcomeLayout: React.FC = () => {
       setExtraStyle({ position: 'absolute' })
     },
     onRest: () => {
+      animating.current = false
       setExtraStyle({ position: 'relative' })
     }
-  })
+   })
+  const main = useRef<HTMLElement>(null)
+  const { direction } = useSwipe(main, { onTouchStart: e => e.preventDefault() })
+  const nav = useNavigate()
+  useEffect(() => {
+    if (direction === 'left') {
+      if (animating.current) { return }
+      animating.current = true
+      nav(linkMap[location.pathname])
+    }
+  },[direction, location.pathname, linkMap])
   return (
     <div className="bg-#5f34bf" h-screen flex flex-col items-stretch pb-16px>
       <header shrink-0 text-center pt-64px>
-        <img src={logo} w-64px h-69px/>
+        <img src={logo} w-64px h-69px />
         <h1 text="#D4D4EE" text-32px>山竹记账</h1>
       </header>
-      <main shrink-1 grow-1 relative >
+      <main shrink-1 grow-1 relative ref={main}>
         {transitions((style, pathname) =>
           <animated.div key={pathname} style={{ ...style, ...extraStyle }} w="100%" h="100%" p-16px flex>
-          <div grow-1 bg-white flex justify-center items-center rounded-8px>
-            {map.current[pathname]}
-          </div>
+            <div grow-1 bg-white flex justify-center items-center rounded-8px>
+              {map.current[pathname]}
+            </div>
           </animated.div>
         )}
       </main>
