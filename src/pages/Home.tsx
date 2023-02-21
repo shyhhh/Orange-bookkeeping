9eabc1f950a77f9e8491e615a12301b8acfa95b6
shyhhh <hhhtheshy@gmail.com>
2023-02-21 15:46:13 +0800

    封装 Loading.tsx 组件

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,7 +16,7 @@ export const Home: React.FC = () => {
   const isLoadingItems = meData && !itemsData && !itemsError
 
   if (isLoadingMe || isLoadingItems) {
-    return <Loading />
+    return <Loading className="h-screen" />
   }
 
   if (itemsData?.resources[0]) {
