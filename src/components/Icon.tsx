803850154cad02928fcecdd73fd2512c5b2246e2
shyhhh <hhhtheshy@gmail.com>
2023-02-21 15:29:16 +0800

    使用 import 确认 css 顺序

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,3 +1,12 @@
-export const Icon: React.FC = () => {
-  return <div>Icon </div>
+import c from 'classnames'
+interface Props {
+  className?: string
+  name: string
+}
+export const Icon: React.FC<Props> = ({ name,className }) => {
+  return (
+    <svg className={c(className, 'h-icon')}>
+      <use xlinkHref={`#${name}`}></use>
+    </svg>
+  )
 }
