4be3f9b1d8ec2375bd1d170031be5d85fcdf55be
shyhhh <hhhtheshy@gmail.com>
2023-02-26 16:25:09 +0800

    抽离出 Tabs.tsx 泛型组件

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -10,8 +10,8 @@ export const TopNav: React.FC<Props> = ({ title = '橙子记账', icon }) => {
       <span w-24px h-24px mr-16px flex justify-center items-center
         children-max-w="100%" children-max-h="100%">
         {icon}
-        </span>
-     <h1 text-24px> {title}</h1>
+      </span>
+      <h1 text-24px> {title}</h1>
     </div>
   )
 }
