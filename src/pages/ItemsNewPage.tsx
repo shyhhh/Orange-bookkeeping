9b6f604fa9adbe41b09927f7237d1ef74d7a756f
shyhhh <hhhtheshy@gmail.com>
2023-02-27 15:26:58 +0800

    完成顶部导航栏

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,13 +1,23 @@
+import { useState } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
+import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
 
+type ItemKind = 'income' | 'expenses'
 export const ItemsNewPage: React.FC = () => {
+  const tabItems: { key: ItemKind; text: string }[]
+    = [{ key: 'expenses', text: '支出' }, { key: 'income', text: '收入' }]
+  const [tabItem, setTabItem] = useState<ItemKind>('expenses')
   return (
     <div>
       <Gradient>
-        <TopNav title='记一笔' icon= {
+        <TopNav title='记一笔' icon={
           <Icon name="back" className='w-24px h-24px' />} />
+        <Tabs tabItems={tabItems} value={tabItem}
+          onChange={(item) => { setTabItem(item) }}
+          className="children-flex-1 text-center"
+        />
       </Gradient>
     </div>
   )
