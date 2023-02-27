2312d2e7b93b1d2b3213b549c103e8b3d9841154
shyhhh <hhhtheshy@gmail.com>
2023-02-27 23:05:05 +0800

    在 main.js 中调用 vh-check

--- uno.config.ts
+++ uno.config.ts
@@ -13,6 +13,9 @@ export default defineConfig({
     'h-form-label': 'text-18px mb-8px',
   },
   safelist: [],
+  rules: [
+    ['h-screen', { height: 'calc(100vh - var(--vh-offset, 0px))' }]
+  ],
   presets: [
     presetUno(),
     presetAttributify(),
