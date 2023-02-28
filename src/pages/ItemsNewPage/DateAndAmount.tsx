a2844cd0eabc292545bc6f08452fec82631ca634
shyhhh <hhhtheshy@gmail.com>
2023-02-28 22:39:47 +0800

    封装 time 类

--- src/pages/ItemsNewPage/DateAndAmount.tsx
+++ src/pages/ItemsNewPage/DateAndAmount.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Datepicker } from '../../components/Datepicker'
 import { Icon } from '../../components/Icon'
 import { usePopup } from '../../hooks/usePopup'
 
@@ -7,61 +7,7 @@ type Props = {
 }
 export const DateAndAmount: React.FC<Props> = (props) => {
   const { className } = props
-  const [isTouching, setIsTouching] = useState(false)
-  const [lastY, setLastY] = useState(-1)
-  const [translateY, setTranslateY] = useState(0)
-  const { popup, toggle } = usePopup(true, <div h="50vh" overflow-hidden relative
-    onTouchStart={(e) => {
-      setIsTouching(true)
-      setLastY(e.touches[0].clientY)
-    }}
-    onTouchMove={(e) => {
-      if (isTouching) {
-        const y = e.touches[0].clientY
-        const dy = y - lastY
-        setTranslateY(translateY + dy)
-        setLastY(y)
-      }
-    }}
-    onTouchEnd={() => {
-      const remainder = translateY % 36
-      let y = translateY - remainder
-      if (Math.abs(remainder) > 18) {
-        y += 36 * (remainder > 0 ? 1 : -1)
-      }
-      setTranslateY(y)
-      setIsTouching(false)
-    }}
-  >
-    <div b-1 b-red h-36px absolute top="[calc(50%-18px)]" w-full />
-    <div absolute top="[calc(50%-18px-108px)]" w-full>
-      <ol style={{ transform: `translateY(${translateY}px)` }}
-        children-h-36px text-center children-leading-36px>
-        <li>2000</li>
-        <li>2001</li>
-        <li>2002</li>
-        <li>2003</li>
-        <li>2004</li>
-        <li>2005</li>
-        <li>2006</li>
-        <li>2007</li>
-        <li>2008</li>
-        <li>2009</li>
-        <li>2010</li>
-        <li>2000</li>
-        <li>2001</li>
-        <li>2002</li>
-        <li>2003</li>
-        <li>2004</li>
-        <li>2005</li>
-        <li>2006</li>
-        <li>2007</li>
-        <li>2008</li>
-        <li>2009</li>
-        <li>2010</li>
-      </ol>
-    </div>
-  </div>)
+  const { popup, toggle } = usePopup(true, <Datepicker />)
   return (
     <>
       {popup}
