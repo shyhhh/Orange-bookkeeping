dc9ad350a3180ca3b6c318d1c4924daf416d3ac0
shyhhh <hhhtheshy@gmail.com>
2023-02-28 15:07:41 +0800

    封装 usePopup 组件

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -21,14 +21,14 @@ export const TopMenu: React.FC<Props> = ({ onClickMask, visible }) => {
     opacity: visible ? 1 : 0,
     transform: visible ? 'translateX(0%)' : 'translateX(-100%)',
   })
-  const styles = {
+  const maskStyles2 = {
     ...maskStyles,
     visibility: (maskVisible ? 'visible' : 'hidden') as 'visible' | 'hidden'
   }
   return (
     <>
       <animated.div fixed top-0 left-0 w="100%" h="100%" className="bg-black:75"
-        z="[calc(var(--z-menu)-1)]" onClick={onClickMask} style={styles}
+        z="[calc(var(--z-menu)-1)]" onClick={onClickMask} style={maskStyles2}
       />
       <animated.div fixed top-0 left-0 w="70vw" max-w-20em h-screen flex flex-col
         z="[var(--z-menu)]" style={menuStyles}>
