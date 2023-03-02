fdbc052ae2c37da7227f03b4c0f280d8ce45fd71
shyhhh <hhhtheshy@gmail.com>
2023-03-02 22:06:48 +0800

    用户点击确认就能选择日期了

--- src/pages/ItemsNewPage/DateAndAmount.tsx
+++ src/pages/ItemsNewPage/DateAndAmount.tsx
@@ -1,13 +1,18 @@
+import { useState } from 'react'
 import { Datepicker } from '../../components/Datepicker'
 import { Icon } from '../../components/Icon'
 import { usePopup } from '../../hooks/usePopup'
+import { time } from '../../lib/time'
 
 type Props = {
   className?: string
 }
 export const DateAndAmount: React.FC<Props> = (props) => {
+  const [date, setDate] = useState(new Date())
   const { className } = props
-  const { popup, toggle } = usePopup(true, <Datepicker onChange={d => window.console.log(d.toLocaleString())} />)
+  const { toggle, popup, hide } = usePopup(false, <Datepicker
+    onConfirm={d => { setDate(d); hide() }}
+    onCancel={() => hide()} />)
   return (
     <>
       {popup}
@@ -15,7 +20,7 @@ export const DateAndAmount: React.FC<Props> = (props) => {
         <div flex p-t-15px p-b-16px px-16px border-t-1px border-t="#ddd" gap-x-8px items-center>
           <span flex items-center gap-x-8px onClick={toggle}>
             <Icon name="calendar" className="w-24px h-24px grow-0 shrink-0" />
-            <span grow-0 shrink-0 text-12px color="#999">2001-02-03</span>
+            <span grow-0 shrink-0 color="#999">{time(date).format()}</span>
           </span>
           <code grow-1 shrink-1 text-right color="#53A867">123456789.01</code>
         </div>
