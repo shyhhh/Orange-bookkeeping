c6d50cef9d22c5c6516a3a14d32636026d1be507
shyhhh <hhhtheshy@gmail.com>
2023-03-10 01:30:05 +0800

    基本完成 新建标签 页面的样式

--- src/pages/TagsNewPage.tsx
+++ src/pages/TagsNewPage.tsx
@@ -1,35 +1,44 @@
 import { useState } from 'react'
 import { emojis } from '../lib/emojis'
+import { Gradient } from '../components/Gradient'
+import { TopNav } from '../components/TopNav'
+import { Icon } from '../components/Icon'
+import s from './TagsNewPage.module.scss'
 
 export const TagsNewPage: React.FC = () => {
   const onSubmit = () => { }
   const [emojiKind, setEmojiKind] = useState('表情')
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <div>
-          <span>标签名</span>
+      <Gradient className="grow-0 shrink-0">
+        <TopNav title="新建标签" icon={<Icon name="back" />} />
+      </Gradient>
+      <form onSubmit={onSubmit} px-16px py-32px flex flex-col gap-y-8px>
+        <div flex flex-col gap-y-8px>
+          <span text-18px>标签名</span>
           <input h-input-text />
-          <span text-red>标签名太长</span>
+          <span text-red text-12px>标签名太长</span>
         </div>
-        <div>
-          <span>符号 😀</span>
-          <div>
-            <div flex>
+        <div flex flex-col gap-y-8px>
+          <span text-18px>符号 <span text-24px>😀</span></span>
+          <div b-1 b="#5C33BE" rounded-8px>
+            <div flex p-8px gap-x-16px overflow-auto text="#999">
               {emojis.map(title =>
-                <span onClick={() => setEmojiKind(title.name)}>
-                  {title.name}
-                </span>)}
+                <span whitespace-nowrap key={title.name}
+                  className={title.name === emojiKind ? s.selectedTag : ''}
+                  onClick={() => setEmojiKind(title.name)}>{title.name}</span>)}
             </div>
-            <div>
+            <div text-24px p-t-8px p-b-16px h-400px overflow-auto text-center>
               {emojis.map(emoji =>
-                <div key={emoji.name} style={{ display: emoji.name === emojiKind ? '' : 'none' }}>
-                  {emoji.chars}
+                <div key={emoji.name} style={{ display: emoji.name === emojiKind ? '' : 'none' }}
+                  grid grid-cols="[repeat(auto-fit,34px)]" grid-rows="[repeat(auto-fit,34px)]"
+                  justify-center>
+                  {emoji.chars.map(char => <span>{char}</span>)}
                 </div>)}
             </div>
           </div>
         </div>
-        <p>记账时长按标签，即可进行编辑</p>
+        <p text-center py-24px>记账时长按标签，即可进行编辑</p>
         <div>
           <button h-btn>确定</button>
         </div>
