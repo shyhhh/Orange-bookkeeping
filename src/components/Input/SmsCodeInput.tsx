1c80884feeda27c48a7f6d025640e39d09272ddf
shyhhh <hhhtheshy@gmail.com>
2023-03-12 01:38:46 +0800

    倒计时的 bug 解决（但还是有可能间隔超过 1 秒）

--- src/components/Input/SmsCodeInput.tsx
+++ src/components/Input/SmsCodeInput.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 type Props = {
   value?: string
@@ -6,34 +6,44 @@ type Props = {
   onChange?: (value: string) => void
   request?: () => Promise<unknown>
 }
+const maxCount = 60
 export const SmsCodeInput: React.FC<Props> = (props) => {
   const { value, placeholder, onChange, request } = props
-  const [count, setCount] = useState(5)
-  const [started, setStarted] = useState(false)
+  const [started, setStarted] = useState<Date>()
+  const [count, setCount] = useState(maxCount)
+  const timer = useRef<number>()
   const onClick = async () => {
     if (!request) { return }
     await request?.()
-    // 开始倒计时
-    setStarted(true)
+    setStarted(new Date())
+  }
+  const clearTimer = () => {
+    if (timer.current) {
+      window.clearInterval(timer.current)
+      timer.current = undefined
+    }
   }
   useEffect(() => {
-    if (!started) { return }
-    if (count === -1) {
-      setStarted(false)
-      setCount(5)
+    if (!started) {
+      clearTimer()
       return
     }
-    const timer = setTimeout(() => {
-      setCount(count - 1)
+    timer.current = window.setInterval(() => {
+      const seconds = Math.round((new Date().getTime() - started.getTime()) / 1000)
+      const count = maxCount - seconds
+      if (count < 0) { setStarted(undefined) }
+      setCount(count)
     }, 1000)
-    return () => window.clearTimeout(timer)
-  }, [count, started])
+    return clearTimer
+  }, [started])
   return (
     <div flex gap-x-16px>
       <input shrink-1 h-input-text type="text" placeholder={placeholder} max-w="[calc(40%-8px)]"
         value={value} onChange={e => onChange?.(e.target.value)} />
       {started
-        ? <button type='button' max-w="[calc(60%-8px)]" shrink-0 h-btn>{count}</button>
+        ? <button type="button" max-w="[calc(60%-8px)]" shrink-0 h-btn disabled text-gray>
+          {count}秒后可重发
+        </button>
         : <button type='button' max-w="[calc(60%-8px)]" shrink-0 h-btn onClick={onClick}>发送验证码</button>
       }
     </div>
