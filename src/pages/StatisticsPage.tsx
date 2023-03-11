71dfa632543242d13fd19913d76d111fc8ba8130
shyhhh <hhhtheshy@gmail.com>
2023-03-11 11:41:10 +0800

    创建 RankChart 组件，样式基本完成，待调整。

--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -3,6 +3,7 @@ import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { LineChart } from '../components/LineChart'
 import { PieChart } from '../components/PieChart'
+import { RankChart } from '../components/RankChart'
 import type { TimeRange } from '../components/TimeRangePicker'
 import { TimeRangePicker } from '../components/TimeRangePicker'
 import { TopNav } from '../components/TopNav'
@@ -42,10 +43,15 @@ export const StatisticsPage: React.FC = () => {
     { date: '2000-01-31', value: 10000 },
   ].map(item => ({ x: item.date, y: item.value / 100 }))
   const items2 = [
-    { tag: '吃饭', amount: 10000 },
-    { tag: '打车', amount: 20000 },
-    { tag: '买皮肤', amount: 68800 },
-  ].map(item => ({ x: item.tag, y: item.amount / 100 }))
+    { tag: { name: '吃饭', sign: '😨' }, amount: 10000 },
+    { tag: { name: '打车', sign: '🥱' }, amount: 20000 },
+    { tag: { name: '买皮肤', sign: '💖' }, amount: 68800 },
+  ].map(item => ({ x: item.tag.name, y: item.amount / 100 }))
+  const items3 = [
+    { tag: { name: '吃饭', sign: '😨' }, amount: 10000 },
+    { tag: { name: '打车', sign: '🥱' }, amount: 20000 },
+    { tag: { name: '买皮肤', sign: '💖' }, amount: 68800 },
+  ].map(item => ({ name: item.tag.name, value: item.amount, sign: item.tag.sign }))
   return (
     <div>
       <Gradient>
@@ -55,7 +61,8 @@ export const StatisticsPage: React.FC = () => {
       </Gradient>
       <TimeRangePicker selected={timeRange} onSelect={setTimeRange} />
       <LineChart className='h-120px' items={items} />
-      <PieChart className="h-260px" items={items2} />
+      <PieChart className="h-260px m-t-16px" items={items2} />
+      <RankChart className="m-t-8px" items={items3} />
     </div>
   )
 }
