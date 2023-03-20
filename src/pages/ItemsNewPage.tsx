eeece9ce9ce49c7136a8b86a9dd857f8fdeb2c0b
shyhhh <hhhtheshy@gmail.com>
2023-03-21 00:19:17 +0800

    收集新建 item 表单的 kind 字段

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -4,6 +4,7 @@ import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
+import { useCreateItemStore } from '../stores/useCreateItemStore'
 import { Tags } from './ItemsNewPage/Tags'
 import { DateAndAmount } from './ItemsNewPage/DateAndAmount'
 import s from './ItemsNewPage.module.scss'
@@ -15,17 +16,19 @@ export const ItemsNewPage: React.FC = () => {
       { key: 'income', text: '收入', element: <Tags kind="income" /> }
     ]
   const [tabItem, setTabItem] = useState<Item['kind']>('expenses')
+  const { data, error, setData, setError } = useCreateItemStore()
   return (
     <div className={s.wrapper} h-screen flex flex-col>
       <Gradient className="grow-0 shrink-0">
         <TopNav title='记一笔' icon={
           <Icon name="back" className='w-24px h-24px' />} />
       </Gradient>
-      <Tabs tabItems={tabItems} value={tabItem}
-        onChange={(item) => { setTabItem(item) }}
+      <Tabs tabItems={tabItems} value={data.kind!}
+        onChange={(tabItem) => { setData({ kind: tabItem }) }}
         className="text-center grow-1 shrink-1 overflow-hidden"
         classPrefix='itemsNewPage'
       />
+      <div>{JSON.stringify(data)}</div>
       <DateAndAmount className="grow-0 shrink-0" />
     </div>
   )
---
+++ src/stores/useCreateItemStore.tsx
@@ -0,0 +1,45 @@
+import create from 'zustand'
+import type { FormError } from '../lib/validate'
+
+type Data = Tag
+
+interface CreateItem {
+  data: Partial<Data>
+  error: FormError<Data>
+  setData: (data: Partial<Data>) => void
+  setError: (error: Partial<FormError<Data>>) => void
+}
+
+export const useCreateItemStore = create<CreateItem>((set, get) => (
+  {
+    data: {
+      kind: 'expenses',
+      tag_ids: [],
+      happen_at: '',
+      amount: 0
+    },
+    error: {
+      kind: [],
+      tag_ids: [],
+      happen_at: [],
+      amount: []
+    },
+    setData: (data: Partial<Data>) => {
+      set(state => ({
+        ...state,
+        data: {
+          ...state.data,
+          ...data
+        }
+      }))
+    },
+    setError: (error: Partial<FormError<Data>>) => {
+      set(state => ({
+        ...state,
+        error: {
+          ...error
+        }
+      }))
+    }
+  }
+))
