cc28f081a4d27be1987311f429b39d00661e9ad9
shyhhh <hhhtheshy@gmail.com>
2023-03-22 00:53:07 +0800

    使得新建 item 表单可以提交

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -12,8 +12,7 @@ import s from './ItemsNewPage.module.scss'
 export const ItemsNewPage: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data, error, setData, setError } = useCreateItemStore()
-  const tabItems: { key: Item['kind']; text: string; element?: ReactNode }[]
-    = [
+  const tabItems: { key: Item['kind']; text: string; element?: ReactNode }[] = [
       {
         key: 'expenses', text: '支出', element:
           <Tags kind="expenses" value={data.tag_ids} onChange={(ids) => setData({ tag_ids: ids })} />
@@ -22,9 +21,12 @@ export const ItemsNewPage: React.FC = () => {
         key: 'income', text: '收入', element:
           <Tags kind="income" value={data.tag_ids} onChange={(ids) => setData({ tag_ids: ids })} />
       }
-    ]
+  ]
+  const onSubmit = () => {
+    window.console.log('你要提交是吧')
+  }
   return (
-    <div className={s.wrapper} h-screen flex flex-col>
+    <div className={s.wrapper} h-screen flex flex-col onSubmit={onSubmit}>
       <Gradient className="grow-0 shrink-0">
         <TopNav title='记一笔' icon={
           <Icon name="back" className='w-24px h-24px' />} />
@@ -37,7 +39,7 @@ export const ItemsNewPage: React.FC = () => {
       <div>{JSON.stringify(data)}</div>
       <ItemAmount className="grow-0 shrink-0" itemDate={
         <ItemDate value={data.happen_at} onChange={(happen_at) => setData({ happen_at })} />
-      } value={data.amount} onChange={(amount) => setData({ amount })} />
+      } value={data.amount} onChange={(amount) => setData({ amount })} onSubmit={ onSubmit} />
     </div>
   )
 }
