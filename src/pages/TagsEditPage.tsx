c3548bba48833f9e041d58be8db6c4c3d7d6cbfe
shyhhh <hhhtheshy@gmail.com>
2023-03-25 00:49:16 +0800

    添加删除 tag 功能

--- src/pages/TagsEditPage.tsx
+++ src/pages/TagsEditPage.tsx
@@ -1,17 +1,32 @@
+import { useNavigate, useParams } from 'react-router-dom'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { TopNav } from '../components/TopNav'
+import { useAjax } from '../lib/ajax'
 import { TagForm } from './TagsNewPage/TagForm'
 
 export const TagsEditPage: React.FC = () => {
+  const confirmable = (fn: () => void) => () => {
+    const result = window.confirm('确定要删除吗？')
+    if (result) { fn() }
+  }
+  const { id } = useParams()
+  const { destroy } = useAjax({ showLoading: true, handleError: true })
+  const nav = useNavigate()
+  const onDelete = confirmable(async () => {
+    if (!id) { throw new Error('id 不能为空') }
+    await destroy(`/api/v1/tags/${id}`).catch((error) => { window.alert('删除失败'); throw error })
+    window.alert('删除成功')
+    nav('/items/new')
+  })
   return (
     <div>
       <Gradient className="grow-0 shrink-0">
         <TopNav title="查看标签" icon={<Icon name="back" />} />
       </Gradient>
-      <TagForm type='edit'/>
+      <TagForm type='edit' />
       <div px-16px p-b-32px>
-        <button h-btn bg="#E10505">删除</button>
+        <button h-btn bg="#E10505" onClick={onDelete}>删除</button>
       </div>
     </div>
   )
