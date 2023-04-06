eaa49d47670616e49e1fab7b2ea3cc0901a27406
shyhhh <hhhtheshy@gmail.com>
2023-04-06 19:37:23 +0800

    Vite 中转代理服务

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,27 @@ import UnoCSS from 'unocss/vite'
 import { viteMockServe } from 'vite-plugin-mock'
 import { svgsprites } from './vite_plugins/svgsprites'
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  server: {
-    host: '0.0.0.0'
-  },
-  define: {
-    isDev: command === 'serve'
-  },
-  base: "./",
-  assetsDir: "assets",
-  plugins: [
-    UnoCSS(),
-    react(),
-    viteMockServe(),
-    svgsprites({ noOptimizeList: ['logo', 'chart', 'category', 'export', 'noty', 'calendar'] })
-  ]
-}))
+// @ts-expect-error
+export default defineConfig((env) => {
+  const { command } = env
+  return {
+    server: {
+      // host: '0.0.0.0'
+      proxy: {
+        '/api/': {
+          target: 'http://121.196.236.94:8080/',
+          changeOrigin: false,
+        },
+      }
+    },
+    define: {
+      isDev: command === 'serve'
+    },
+    plugins: [
+      UnoCSS(),
+      react(),
+      viteMockServe(),
+      svgsprites({ noOptimizeList: ['logo', 'chart', 'category', 'export', 'noty', 'calendar'] })
+    ]
+  }
+})
