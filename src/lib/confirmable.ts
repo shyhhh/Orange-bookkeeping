5a47fd89e7d03f8235cf1f1dde16808c925705a6
shyhhh <hhhtheshy@gmail.com>
2023-04-08 00:16:14 +0800

    提供退出登录功能

--- src/lib/confirmable.ts
+++ src/lib/confirmable.ts
@@ -1,4 +1,4 @@
-export const confirmable = (fn: () => void) => () => {
-  const result = window.confirm('确定要删除吗？')
+export const confirmable = (title: string, fn: () => void) => () => {
+  const result = window.confirm(title)
   if (result) { fn() }
 }
