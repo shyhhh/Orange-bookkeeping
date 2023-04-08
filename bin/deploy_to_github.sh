c95505d087b85a4d96a0f436540379c72ad8ba84
shyhhh <hhhtheshy@gmail.com>
2023-04-08 18:01:29 +0800

    自动化可交互式脚步

--- bin/deploy_to_github.sh
+++ bin/deploy_to_github.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
+function set_env {
+  name=$1
+  hint=$2
+  [[ ! -z "${!name}" ]] && return
+  while [ -z "${!name}" ]; do
+    [[ ! -z "$hint" ]] && echo "> 请输入 $name: $hint" || echo "> 请输入 $name:"
+    read $name
+  done
+  if [ ! -s ./.env ]; then
+    echo "export $name=\"${!name}\"" > ./.env
+  else
+    sed -i "1s/^/export $name=\"${!name}\"\n/" ./.env
+  fi
+
+  echo "${name} 已保存至 ./.env 。如果需要修改，请自行编辑 ./.env"
+}
+
+touch ./.env && source ./.env
+set_env user "GitHub 用户名"
+set_env repo "GitHub 仓库名"
+
 rm -rf dist
-npm run build -- --base=/Orange-bookkeeping/
+npm run build -- --base "/$repo"
 cd dist
 git init
 git checkout -B main
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:shyhhh/Orange-bookkeeping.git main:preview-pages
-echo "================部署成功========================"
+git push -f git@github.com:$user/$repo.git main:preview-pages
+git home
 cd -
-·
