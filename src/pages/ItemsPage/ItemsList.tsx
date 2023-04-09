da88ea0e08a0751624e8f4f0cdb20906913a19a5
shyhhh <hhhtheshy@gmail.com>
2023-04-09 13:02:47 +0800

    f修复 sign

--- src/pages/ItemsPage/ItemsList.tsx
+++ src/pages/ItemsPage/ItemsList.tsx
@@ -54,7 +54,7 @@ export const ItemsList: React.FC<Props> = (props) => {
                 border-b-1 b-solid b="#EEE">
                 <div row-start-1 col-start-1 row-end-3 col-end-2 text-24px w-48px h-48px
                   bg="#D8D8D8" rounded="50%" flex justify-center items-center>
-                  🦁️
+                  {item.tags?.[0].sign}
                 </div>
                 <div row-start-1 col-start-2 row-end-2 col-end-3>
                   {item.tags?.[0].name}
