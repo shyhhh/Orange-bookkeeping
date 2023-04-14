1f9fb19894e246cca718b2d66867d2d1a84ce1a8
shyhhh <hhhtheshy@gmail.com>
2023-04-14 14:36:56 +0800

    添加 敬请期待 svg

--- src/pages/ComingSoonPage.tsx
+++ src/pages/ComingSoonPage.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from 'react-router-dom'
-import { Icon } from '../components/Icon'
+import groovyWalkAnimation from '../assets/json/ComingSongPage.json'
+import { WelcomeScreen } from '../components/WelcomeScreen'
 export const ComingSoonPage: React.FC = () => {
   const nav = useNavigate()
   return (
     <div flex justify-center items-center flex-col gap-y-24px py-48px h-screen px-48px>
-      <Icon name="pig" className="w-128px h-128px" />
-      <h1>敬请期待</h1>
+      <WelcomeScreen animationData={groovyWalkAnimation} />
+      <div text='#333'>敬请期待 ~</div>
       <button h-btn onClick={() => nav(-1)}>返回</button>
     </div>
   )
