12ac925b61f58638b87e88326207bc9efdd5a032
shyhhh <hhhtheshy@gmail.com>
2023-04-17 17:46:38 +0800

    deploy

---
+++ assets/WelcomeScreen-3343275a.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-420191c7.js";import{bR as e,bu as m}from"./vendor-7927c036.js";const p=a=>{const{className:o,animationData:t}=a;return s(e,{className:m(o,"w-300px","h-300px","m-t--50px"),animationData:t,loop:!0})};export{p as W};
