32afeadcf8edf765f1e76ee4fdb9d4c51f8ebc5c
shyhhh <hhhtheshy@gmail.com>
2023-04-17 17:34:06 +0800

    连接 到 gitee

--- bin/deploy_to_gitee.sh
+++ bin/deploy_to_gitee.sh
@@ -1,9 +1,10 @@
 rm -rf dist
-npm run build -- --base orangebk
+npm run build -- --base /orangebk
 cd dist
 git init
 git checkout -B main
 git add -A
 git commit -m 'deploy'
+git remote add gitee git@gitee.com:he-honghe/orangebk.git
 git push -f gitee git@gitee.com:he-honghe/orangebk.git main:preview-pages
 cd -
